f88bf3d8671fb6a9e6796788f4abafa44efd1424
swordgit <swordgit@gmail.com>
2022-04-22 16:57:16 -0400

    Adding mocked test case

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UnitTest
+{
+   @Test
+   public void testing(){
+      assertTrue(true);
+   }
+}
